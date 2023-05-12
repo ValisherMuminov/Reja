e050adf2b9311cee61936c27ae662c813639038f
Valisher Muminov <valishaermuminov@gmail.com>
2023-05-13 05:27:28 +0900

    feat: mongoDB ulanish

--- server.js
+++ server.js
@@ -1,51 +1,30 @@
 
-console.log("Web Serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();   
+
 const http = require("http"); 
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
-// 1 Kirish code
+let db;
+const connectionString =
+  "mongodb+srv://valishermuminov:iBBmiNi2Hvc8hFMi@cluster0.hiakcdu.mongodb.net/reja";
 
-app.use(express.static("public")); 
-
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-
-// 2 Session code
-
-// 3 Views code
-
-app.set("views", "views"); 
-app.set("view engine", "ejs"); 
-
-// 4 Routing
-
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", function (req, res) {
-  res.render("author", { user: user });
-});
-app.get("/", function (req, res) {
-  res.render("reja");
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) console.log("ERROR on connection Mongodb");
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(
+                `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+            );
+        });
+    }
 });
 
 
-const server = http.createServer(app);  //single thread shu app hamma req res larga javob beradi shuni band qilmaslik un 1 usul: callback dan faydalanish
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
 
