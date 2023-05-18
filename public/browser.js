839227e646667a6cfe207f21c683621fee0bdd98
Valisher Muminov <valishaermuminov@gmail.com>
2023-05-18 14:38:46 +0900

    feat: Frontend CRUD qismlari yakunlandi

--- public/browser.js
+++ public/browser.js
@@ -1,18 +1,23 @@
-console.log("Frontend JS ishga tushdi");
+console.log("FrontEnd JS ishga tushiramiz");
 
 function itemTemplate(item) {
-  return `<li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-        <span class="item-text">${item.reja}</span>
+  return `<li  font-size: 20px; color: white; font-weight: 500;"
+        class="list-group-item list-group-item-info d-flex align-items-center justify-content-between"
+        >
+        <span class="item-text"> ${item.reja}</span>
         <div>
-          <button
-           data-id="${item._id}"
-           class="edit-me btn btn-secondary btn-sm mr-1" >O'zgartirish</button>
-          <button
-           data-id="${item._id}"
-           class="delete-mebtn btn-danger btn-sm" >O'chirish</button>
+            <button 
+              data-id="${item._id}>" 
+              class="edit-me btn btn-secondary btn-sm mr-1">
+              Ozgartirish
+            </button>
+            <button data-id="${item._id}" 
+              class="delete-me btn btn-danger btn-sm">
+              Ochirish
+            </button>
         </div>
-      </li>`;
-};
+        </li>`;
+}
 
 let createField = document.getElementById("create-field");
 
@@ -29,6 +34,54 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       createField.focus();
     })
     .catch((err) => {
-      console.log("Iltimos qayta xarakat qiling!");
+      console.log("Iltimos qaytadan harakat qiling!");
     });
+});
+
+document.addEventListener("click", function (e) {
+  // delete code lari
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisiz?")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((respose) => {
+          console.log(respose.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
+  }
+
+  // edit code lari
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt(
+      "O'zgartirish kiriting",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
+  }
+});
+
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", { delete_all: true }).then((respose) => {
+    alert(respose.data.state);
+    document.location.reload();
+  });
 });
